5ea6761d82dfabd155dfe85d118174b13544d048
DrevniyMagistr <43793076+DrevniyMagistr@users.noreply.github.com>
2022-02-07 14:00:30 +0000

    Deploying to gh-pages from @ DrevniyMagistr/goit-js-hw-10@68a00a879b601a61eaae5e69bca58c59760e2ea4 🚀

--- src.15f69a57.js
+++ src.15f69a57.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./css/styles.css");var s=require("./js/fetchCountries");const e=300;
 },{"./css/styles.css":"krre","./js/fetchCountries":"irxn"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.15f69a57.js.map
+//# sourceMappingURL=/goit-js-hw-10/src.15f69a57.js.map
